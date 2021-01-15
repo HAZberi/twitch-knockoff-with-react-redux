a4dcc01506b58ed20af3873008069507cb506f72
HAZberi <mhazuberi@gmail.com>
2021-01-15 12:53:55 -0700

    connected google api

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="ui pointing menu">
-            <Link className="item" to="/">StreamA</Link>
+        <div className="ui secondary pointing menu">
+            <Link className="active item" to="/">StreamA</Link>
             <div className="right menu">
                 <Link className="item" to="/">All Channels</Link>
             </div>
@@ -13,3 +13,5 @@ const Header = () => {
 }
 
 export default Header;
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+//Client ID
+// 749031414634-8af1nvge4p1qnd6dgjhj13s8sptfksgo.apps.googleusercontent.com
