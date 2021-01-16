7eb21dcef9c0ed28e2e5417fe1e2fc250b6576f1
HAZberi <mhazuberi@gmail.com>
2021-01-15 17:39:35 -0700

    setting up redux for our project

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,19 +1,70 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 
 const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
 
-    useEffect(()=>{
-        window.gapi.load("auth2", ()=>{
-            window.gapi.auth2.init({
-                client_id: "749031414634-8af1nvge4p1qnd6dgjhj13s8sptfksgo.apps.googleusercontent.com",
-                scope: 'email',
-            })
+  useEffect(() => {
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2
+        .init({
+          client_id:
+            "749031414634-8af1nvge4p1qnd6dgjhj13s8sptfksgo.apps.googleusercontent.com",
+          scope: "email",
         })
-    },[])
+        .then((authentication) => {
+          //the argument name in this function is The GoogleAuth Object
+          //authentication = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(authentication.isSignedIn.get());
+          authentication.isSignedIn.listen(onChangeAuth);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  }, []);
 
-    return (
-        <div>Google Auth</div>
-    )
-}
+  const onChangeAuth = () => {
+    setIsSignedIn(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+  };
+
+  const onSignInClick = () => {
+    window.gapi.auth2.getAuthInstance().signIn();
+  };
+  const onSignOutClick = () => {
+    window.gapi.auth2.getAuthInstance().signOut();
+  };
+
+  const renderSignInStatus = () => {
+    if (isSignedIn === null) {
+      return null;
+    } else if (isSignedIn) {
+      return (
+        <button
+          onClick={onSignOutClick}
+          style={{ marginTop: "0.2em" }}
+          className="ui blue google button small"
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else if (!isSignedIn) {
+      return (
+        <button
+          onClick={onSignInClick}
+          style={{ marginTop: "0.2em" }}
+          className="ui blue google button small"
+        >
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    } else {
+      return "No authentication Exists";
+    }
+  };
+
+  return <div>{renderSignInStatus()}</div>;
+};
 
 export default GoogleAuth;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers);
 
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 //Client ID
 // 749031414634-8af1nvge4p1qnd6dgjhj13s8sptfksgo.apps.googleusercontent.com
