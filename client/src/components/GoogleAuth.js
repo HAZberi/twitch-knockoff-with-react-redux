4ad081ed00602bcb0e97982204c0288abb243557
HAZberi <mhazuberi@gmail.com>
2021-01-16 17:25:42 -0700

    converted Google Auth to react redux hooks

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useCallback, useRef } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signIn, signOut } from "../actions";
 
-const GoogleAuth = ({ isSignedIn, signIn, signOut }) => {
+const GoogleAuth = () => {
   //useCallback Hook is used to wrap the callback function to minimize re-renders
   //Important concept or trick regarding useCallback Hook as follows
   //Whenever a callback function is set as a dependency of useEffect
   //Its a good idea to wrap the callback function with useCallback Hook
   //Snippet from article: When the function object is a dependency to other hooks, e.g. useEffect(..., [callback])
+  
+  //Connect is replaced by two separate functions
+  //useSelector is somewhat equivalent to mapStateToProps
+  //useSelector uses strict comparisions (===) instead (==) as done with mapStateToProps
+  const isSignedIn = useSelector(state => state.auth.isSignedIn);
+  //useDispatch basically manually dispatches the actions to reducers
+  const dispatch = useDispatch();
+
+  
   const onChangeAuth = useCallback(
     (isSignedIn) => {
       if (isSignedIn) {
         //This is how we get the google id of current user
-        signIn(googleAuthObject.current.currentUser.get().getId());
+        dispatch(signIn(googleAuthObject.current.currentUser.get().getId()));
       } else {
-        signOut();
+        dispatch(signOut());
       }
     },
-    [signIn, signOut]
+    [dispatch]
   );
 
   //whenever we want a value to persist in our component we use the 'useRef' Hook
@@ -87,8 +96,4 @@ const GoogleAuth = ({ isSignedIn, signIn, signOut }) => {
   return <div>{renderSignInStatus()}</div>;
 };
 
-const mapStateToProps = (state) => {
-  return { isSignedIn: state.auth.isSignedIn };
-};
-
-export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
+export default GoogleAuth;
