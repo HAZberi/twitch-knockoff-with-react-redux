c15273f432a30ff37881ff4f5644ad1911ad305a
HAZberi <mhazuberi@gmail.com>
2021-01-18 02:35:01 -0700

    Creating Fields and displaying simple input

--- client/src/components/videos/VideosCreate.js
+++ client/src/components/videos/VideosCreate.js
@@ -2,10 +2,24 @@ import React from "react";
 import { Form, Field } from "react-final-form";
 
 const VideosCreate = () => {
+
+    const renderInput = () => {
+        return (
+            <input />
+        );
+    }
+
+
     return (
         <Form onSubmit={()=>console.log('form has been submitted')}>
             {(props)=>{
-                console.log(props)
+                console.log(props);
+                return (
+                    <form onSubmit={()=>console.log("Replace this function")}>
+                        <Field name="title" render={renderInput}/>
+                        <Field name="description" render={renderInput}/>
+                    </form>                
+                )
             }}
         </Form>
     )
