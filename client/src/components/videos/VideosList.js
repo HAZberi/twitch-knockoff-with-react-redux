4324fb1e22eb29c7f5835e3f5bd0311e44a4f5cb
HAZberi <mhazuberi@gmail.com>
2021-01-18 20:45:14 -0700

    rendering list

--- client/src/components/videos/VideosList.js
+++ client/src/components/videos/VideosList.js
@@ -1,16 +1,36 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getStreams } from "../../actions";
 
 const VideosList = () => {
 
+  const videosList = useSelector(state => Object.values(state.videos));
   const dispatch = useDispatch();
+
+  const renderList = (videosList) => {
+    return videosList?.map((video) => {
+      return(
+        <div className="item" key={video.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {video.title}
+            <div className="description">
+              {video.description}
+            </div>
+          </div>
+        </div>
+        )
+    }
+    )
+  }
 
   useEffect(()=>{
     dispatch(getStreams());
-  },[dispatch])
+  },[dispatch]);
 
-  return <div>VideosList</div>;
+  return (
+    <div className="ui celled list">{renderList(videosList)}</div>
+  );
 };
 
 export default VideosList;
