44832c154e8b0c886fbd66589949e8b9d8cff285
HAZberi <mhazuberi@gmail.com>
2021-01-18 20:11:05 -0700

    getting streams list from api server BASIC SETUP

--- client/src/components/videos/VideosList.js
+++ client/src/components/videos/VideosList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getStreams } from "../../actions";
 
 const VideosList = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getStreams());
+  },[dispatch])
+
   return <div>VideosList</div>;
 };
 
