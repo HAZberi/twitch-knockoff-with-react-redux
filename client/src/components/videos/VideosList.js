e1361aaf048525154e4ef0d8d226aac119924a15
HAZberi <mhazuberi@gmail.com>
2021-01-20 12:39:18 -0700

    Linking create Stream from videolist

--- client/src/components/videos/VideosList.js
+++ client/src/components/videos/VideosList.js
@@ -1,47 +1,63 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getStreams } from "../../actions";
 
 const VideosList = () => {
-
-  const videosList = useSelector(state => Object.values(state.videos));
-  const currentUserId = useSelector(state => state.auth.userId);
+  const videosList = useSelector((state) => Object.values(state.videos));
+  const { userId: currentUserId, isSignedIn } = useSelector(
+    (state) => state.auth
+  );
   const dispatch = useDispatch();
 
   const renderAdmin = (video) => {
-    if(video.userId === currentUserId){
+    if (video.userId === currentUserId) {
       return (
         <div className="right floated content">
           <button className="ui primary inverted button">Edit</button>
           <button className="ui red inverted button">Delete</button>
         </div>
-      )
+      );
     }
-  }
+  };
   const renderList = (videosList) => {
     return videosList?.map((video) => {
-      return(
+      return (
         <div className="item" key={video.id}>
           {renderAdmin(video)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {video.title}
-            <div className="description">
-              {video.description}
-            </div>
+            <div className="description">{video.description}</div>
           </div>
         </div>
-        )
-    }
-    )
-  }
+      );
+    });
+  };
 
-  useEffect(()=>{
+  const renderCreateButton = (isSignedIn) => {
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/videos/new" className="ui large teal button">
+            Create your own Stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  useEffect(() => {
     dispatch(getStreams());
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
-    <div className="ui celled list">{renderList(videosList)}</div>
+    <>
+      <div className="ui celled list">{renderList(videosList)}</div>
+      <div className="ui right floated content">
+        {renderCreateButton(isSignedIn)}
+      </div>
+    </>
   );
 };
 
