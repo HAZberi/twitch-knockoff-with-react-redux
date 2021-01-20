c0b1083e96b63555bb37be0915e5542559252589
HAZberi <mhazuberi@gmail.com>
2021-01-20 14:57:46 -0700

    component level data fetching for react router

--- client/src/components/videos/VideosEdit.js
+++ client/src/components/videos/VideosEdit.js
@@ -1,31 +1,43 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getStream } from "../../actions"
 //Route params are variables in our pathname like :id -- see <Router />
 //In order to access params with access default props.
 //Since the component is linked with Router TAG we automatically recieve some
 //default props
-//these default props has properties like match, history, pathname etc. 
+//these default props has properties like match, history, pathname etc.
 //toggle the console.log to see default props
 
-
 //At this point we are only able to retrieve the videotoedit if we
-//have been redirected from videoList Homepage where all the data loading 
+//have been redirected from videoList Homepage where all the data loading
 //actually happens for the first time
 //So if we just refresh the edit page we have to re-fetch data from the db
-//So the main point to remeber is 
+//So the main point to remeber is
 //With React=Router, each component needs to be designed to work in isolation
-//(fetch its own data) 
+//(fetch its own data)
 const VideosEdit = (props) => {
-
-  const videoToEdit = useSelector(state=>state.videos[props.match.params.id]);
-
-  console.log(props, videoToEdit);
-  return (
-    <div>
-      VideosEdit
-    </div>
+  const videoId = props.match.params.id;
+  const videoToEdit = useSelector(
+    (state) => state.videos[videoId]
   );
+  const dispatch = useDispatch();
+
+  const renderEditForm = (video) => {
+    if(!video) return <div>Loading...</div>
+    return (
+      <div>{video.title}</div>
+    )
+  }
+  useEffect(()=>{
+    dispatch(getStream(videoId))
+  },[dispatch, videoId])
+
+  //console.log(props, videoToEdit);
+  return (
+    <>
+      {renderEditForm(videoToEdit)}
+    </>
+  )
 };
 
 export default VideosEdit;
