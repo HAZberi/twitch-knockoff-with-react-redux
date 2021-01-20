989d3963fe708306842925d3e8b2545f813457a3
HAZberi <mhazuberi@gmail.com>
2021-01-20 14:34:50 -0700

    Selecting the video to edit

--- client/src/components/videos/VideosEdit.js
+++ client/src/components/videos/VideosEdit.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 //Route params are variables in our pathname like :id -- see <Router />
 //In order to access params with access default props.
@@ -6,9 +7,25 @@ import React from "react";
 //default props
 //these default props has properties like match, history, pathname etc. 
 //toggle the console.log to see default props
+
+
+//At this point we are only able to retrieve the videotoedit if we
+//have been redirected from videoList Homepage where all the data loading 
+//actually happens for the first time
+//So if we just refresh the edit page we have to re-fetch data from the db
+//So the main point to remeber is 
+//With React=Router, each component needs to be designed to work in isolation
+//(fetch its own data) 
 const VideosEdit = (props) => {
-  console.log(props);
-  return <div>VideosEdit</div>;
+
+  const videoToEdit = useSelector(state=>state.videos[props.match.params.id]);
+
+  console.log(props, videoToEdit);
+  return (
+    <div>
+      VideosEdit
+    </div>
+  );
 };
 
 export default VideosEdit;
