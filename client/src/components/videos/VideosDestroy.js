6d38ac09b0a2ae305768075f7a696bf653959547
HAZberi <mhazuberi@gmail.com>
2021-01-20 22:00:08 -0700

    abstracting modal content

--- client/src/components/videos/VideosDestroy.js
+++ client/src/components/videos/VideosDestroy.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Modal from "../Modal"
+import Modal from "../Modal";
 
 const VideosDestroy = () => {
+  const renderActions = (
+    <>
+      <button className="ui red button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
-      Delete Stuff here
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={renderActions}
+      />
     </div>
   );
 };
