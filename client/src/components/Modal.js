4afac48d7b7cb8ab3be47a10dda21a56068ec155
HAZberi <mhazuberi@gmail.com>
2021-01-20 21:40:18 -0700

    modal styling and click handlers

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        O Heavy Scene hogia Modal Window ban gai....
+    <div
+      className="ui dimmer modals visible active"
+      onClick={() => history.push("/")}
+    >
+      <div
+        className="ui standard modal visible active"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui red button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector("#modal")
