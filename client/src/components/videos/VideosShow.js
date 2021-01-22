3a327fa444707c22802f0760205bff3951ca2bfe
HAZberi <mhazuberi@gmail.com>
2021-01-21 17:11:44 -0700

    showing up basiv html 5 video player

--- client/src/components/videos/VideosShow.js
+++ client/src/components/videos/VideosShow.js
@@ -1,4 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import flv from "flv.js";
+
+/* flv.js acts like axios or fetch it get the data from a remote server
+  and injects the video in our html 5 video player Simple!!
+*/
+
+
+
+
+
 import { useSelector, useDispatch } from "react-redux";
 import { getStream } from "../../actions";
 
@@ -7,15 +17,19 @@ const VideosShow = (props) => {
   const videoToShow = useSelector((state) => state.videos[videoId]);
   const dispatch = useDispatch();
 
+  const videoPlayer = useRef();
+
   const render = (video) => {
     if (!video) return <div>Loading...</div>;
     return (
       <div>
+        <video ref={videoPlayer} style={{width: "100%"}} controls />
         <h1>{video.title}</h1>
         <h4>{video.description}</h4>
       </div>
     );
   };
+
 
   useEffect(() => {
     dispatch(getStream(videoId));
